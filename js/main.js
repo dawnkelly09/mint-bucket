84bd9cc5f91466fdc5c0f9b97db706952b41fda4
dawnkelly09 <dawnkelly09@gmail.com>
2022-08-13 15:48:36 -0700

    adds connect wallet & logout fx's conct wlt passed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
-const { default: Moralis } = require("moralis");
+/* Moralis init code */
+const serverUrl = "https://mczld76x1lnp.usemoralis.com:2053/server";
+const appId = "tq3TUnN7bIwEaAVKo4T555eQ9AngSOo9DZMn96un";
+Moralis.start({ serverUrl, appId });
 
-const serverUrl = "";
-const appId = "";
-Moralis.start({serverUrl, appID});
+/* Authentication code */
+async function login() {
+    let user = Moralis.User.current();
+    if (!user) {
+      user = await Moralis.authenticate({
+        signingMessage: "Log in using Moralis",
+      })
+        .then(function (user) {
+          console.log("logged in user:", user);
+          console.log(user.get("ethAddress"));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+  
+  async function logOut() {
+    await Moralis.User.logOut();
+    console.log("logged out");
+  }
+  
+  document.getElementById("connect-wallet").onclick = login;
+  //TODO: add Logout button on the photo album page
+  //document.getElementById("btn-logout").onclick = logOut;
